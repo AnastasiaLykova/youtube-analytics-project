4d47e464350ee3fb4a8ce0aba17d8af1514d2dc2
anastasia_lykova <anastasia.7@mail.ru>
2023-06-20 20:52:40 +0500

    add Video and PLVideo classes

---
+++ src/video.py
@@ -0,0 +1,23 @@
+from src.channel import Channel
+
+
+class Video:
+    def __init__(self, video_id):
+        self.video_id = video_id
+        youtube = Channel.get_service()
+        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                               id=video_id
+                                               ).execute()
+        self.url = 'https://youtu.be/' + self.video_id
+        self.video_title: str = video_response['items'][0]['snippet']['title']
+        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
+        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+
+    def __str__(self):
+        return f'{self.video_title}'
+
+
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
